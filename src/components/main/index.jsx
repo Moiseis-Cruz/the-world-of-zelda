f9200724b2d1243efeb9a9122ec5c7158ca6410a
Moiseis Cruz <moizzito@gmail.com>
2023-12-04 12:38:28 -0300

    I used the object's methods

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 async function getDatosZelda() {
   const response = await fetch("https://zelda.fanapis.com/api/games");
   const ZeldaData = await response.json();
@@ -7,9 +10,39 @@ async function getDatosZelda() {
 console.log(await getDatosZelda());
 
 export const Main = () => {
+
+    const [ zeldaData, setZeldaData ] = useState({characters: []})
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getDatosZelda()
+
+            setZeldaData({characters: data})
+        }
+        fetchData()
+    },[])
+
   return (
     <>
-      <h1>Título</h1>
+      <section>
+        <ul>
+            {
+                zeldaData.characters.map((item, index) => {
+                    return(
+                        <li key={index}>
+                            <div>
+                                <h1>{item.name}</h1>
+                                <h2>{item.developer}</h2>
+                                <h2>{item.publisher}</h2>
+                                <h4>{item.released_date}</h4>
+                                <p>{item.description}</p>
+                            </div>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+      </section>
     </>
   );
 };
