cef1a895024d3a0c780a89773c1422324264df88
Moiseis Cruz <moizzito@gmail.com>
2023-12-06 18:43:10 -0300

    I created the default global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import "./App.css";
 import { Main } from "./components/main";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
   return(
     <>
+      <GlobalStyles />
       <Main />
     </>
   )
 }
 
 export default App;
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul {
+    list-style: none;
+  }
+`
