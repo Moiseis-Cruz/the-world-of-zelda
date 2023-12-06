2c95f9b494b50c8653c0bd744e4fdfb11f099177
Moiseis Cruz <moizzito@gmail.com>
2023-12-05 21:54:45 -0300

    Code refactoring

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,29 +1,38 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 async function getDatosZelda() {
   const response = await fetch("https://zelda.fanapis.com/api/games");
-  const ZeldaData = await response.json();
-  return ZeldaData.data
+  const datos = await response.json();
+  return datos.data;
 }
 
-console.log(await getDatosZelda());
-
 export const Main = () => {
-  const [zeldaData, setZeldaData] = useState({ characters: [] });
+  const [ zeldaData, setZeldaData] = useState({ games: [] });
+  const [currentPage, setCurrentPage] = useState(1);
 
   const ZeldaList = (props) => {
     return (
       <ul>
-        {props.characters.map((item, index) => {
+        {props.games.map((item, index) => {
           return (
             <li key={index}>
-              <div>
+              <div style={{ border: "5px solid red" }}>
                 <h1>{item.name}</h1>
-                <h2>{item.developer}</h2>
-                <h2>{item.publisher}</h2>
-                <h4>{item.released_date}</h4>
-                <p>{item.description}</p>
+                <h2>
+                  <b>Developer:</b> {item.developer}
+                </h2>
+                <h2>
+                  <b>Publiser: </b>
+                  {item.publisher}
+                </h2>
+                <h4>
+                  <b>Released Date: </b>
+                  {item.released_date}
+                </h4>
+                <p>
+                  <b>Description: </b>
+                  {item.description}
+                </p>
               </div>
             </li>
           );
@@ -34,18 +43,47 @@ export const Main = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getDatosZelda();
+      const zeldaDatos = await getDatosZelda();
 
-      setZeldaData({ characters: data });
+      setZeldaData({ games: zeldaDatos });
     };
     fetchData();
   }, []);
 
+  const nextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const prevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const gamesPerPage = 5; // Número de personagens por página
+  const indexOfLastCharacter = currentPage * gamesPerPage;
+  const indexOfFirstCharacter = indexOfLastCharacter - gamesPerPage;
+  const currentCharacters = zeldaData.games.slice(
+    indexOfFirstCharacter,
+    indexOfLastCharacter
+  );
+
   return (
-    <>
-      <section>
-        {zeldaData.characters.length > 0 ? <ZeldaList characters={zeldaData.characters} /> : "❌❌ !!Não resultado encontrado!! ❌❌"}
-      </section>
-    </>
+    <section>
+      {zeldaData.games.length > 0 ? (
+        <>
+          <ZeldaList games={currentCharacters} />
+          <button onClick={prevPage} disabled={currentPage === 1}>
+            Página Anterior
+          </button>
+          <button
+            onClick={nextPage}
+            disabled={indexOfLastCharacter >= zeldaData.games.length}
+          >
+            Próxima Página
+          </button>
+        </>
+      ) : (
+        "❌❌ Não há nada sobre a franquia ❌❌"
+      )}
+    </section>
   );
 };
